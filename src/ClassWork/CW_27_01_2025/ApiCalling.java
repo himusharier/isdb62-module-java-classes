b864f9bad9bdbd1a11a34deb91a4504612bd4fc4
Sharier Himu <himusharier@gmail.com>
2025-01-27 13:09:33 +0600

    end of class 27-01-2025. next: start a project!

---
+++ src/ClassWork/CW_27_01_2025/ApiCalling.java
@@ -0,0 +1,51 @@
+package ClassWork.CW_27_01_2025;
+
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class ApiCalling {
+    public static void main(String[] args) throws IOException {
+        /*
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://www.uuidtools.com/api/generate/v4/count/10")
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response);
+        */
+
+
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://www.uuidtools.com/api/generate/v4/count/10")
+                .get()
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+
+            if (response.isSuccessful()) {
+                //System.out.println("Response: " + response.body().string());
+                //System.out.println(response.body().string());
+                String allData = response.body().string();
+                String[] dataArray = allData.split(",");
+
+                for (String data : dataArray) {
+                    System.out.println(data);
+                }
+
+            } else {
+                System.out.println("Request failed: " + response.message());
+            }
+        }
+
+
+    }
+}
